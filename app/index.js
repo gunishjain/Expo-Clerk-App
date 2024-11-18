25b95b474474be4a68c6b605fed43b8b4b59d933
gunishjain <gunish.jain@gmail.com>
2024-11-18 10:48:13 +0530

    Setup Project

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Xcel Med Connect</Text>
+      <StatusBar style='auto'/>
+    </View>
+  )
+}
